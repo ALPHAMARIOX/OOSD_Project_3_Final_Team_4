ad0149ca2de72361bfa5e1fc4672d8aeabb7bec3
Lee Harrold <alphamariox@gmail.com>
2016-03-02 23:38:42 -0700

    Whoops

--- src/settings/ToggleSound.java
+++ src/settings/ToggleSound.java
@@ -3,10 +3,12 @@ package settings;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.applet.*;
+import java.awt.CheckboxMenuItem;
 
 public class ToggleSound {
 	
-	AudioClip fx;
+	public AudioClip fx;
+	public CheckboxMenuItem chckbxmntmToggleSound = new CheckboxMenuItem();
 	public ToggleSound() {
 		try {
 			fx = Applet.newAudioClip(new File("./audio/beep.wav").toURI().toURL());
@@ -19,5 +21,12 @@ public class ToggleSound {
 	
 	public void playSound() {
 		fx.play();
+	}
+	
+	public void useSound() {
+		if (chckbxmntmToggleSound.isSelected() == true)
+		{
+			playSound();
+		}
 	}
 }
